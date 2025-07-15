e10c07415a61b0e40c42a66d3803fc2741f49ecb
Stefan Bartl <bartl.stefanwien@gmail.com>
2025-07-15 17:28:04 +0200

    n

---
+++ Terminals/wezterm/config/keybindings.lua
@@ -0,0 +1,95 @@
+---@module 'config.keybindings'
+---@brief Key mappings for WezTerm, including dynamic color scheme switching
+---@version 1.0
+
+local wezterm = require("wezterm")
+
+--@param Config WezTermConfig
+--@return nil
+return function(Config)
+  --- Define custom keybindings
+  Config.keys = {
+    -- Toggle between color schemes
+    {
+      key = "1",
+      mods = "CTRL|ALT",
+      action = wezterm.action.EmitEvent("set-color-hackthebox"),
+    },
+
+    -- STRG + SHIFT + Pfeil links → vorheriger Tab
+    {
+      key = "LeftArrow",
+      mods = "CTRL|SHIFT",
+      action = wezterm.action.ActivateTabRelative(-1),
+    },
+    -- STRG + SHIFT + Pfeil rechts → nächster Tab
+    {
+      key = "RightArrow",
+      mods = "CTRL|SHIFT",
+      action = wezterm.action.ActivateTabRelative(1),
+    },
+
+    {
+      key = "1",
+      mods = "CTRL|SHIFT",
+      action = wezterm.action.ActivateTab(0),
+    },
+    {
+      key = "2",
+      mods = "CTRL|SHIFT",
+      action = wezterm.action.ActivateTab(1),
+    },
+    {
+      key = "4",
+      mods = "CTRL|SHIFT",
+      action = wezterm.action.ActivateTab(2),
+    },
+    {
+      key = "5",
+      mods = "CTRL|SHIFT",
+      action = wezterm.action.ActivateTab(2),
+    },
+    {
+      key = "6",
+      mods = "CTRL|SHIFT",
+      action = wezterm.action.ActivateTab(2),
+    },
+    {
+      key = "7",
+      mods = "CTRL|SHIFT",
+      action = wezterm.action.ActivateTab(2),
+    },
+    {
+      key = "8",
+      mods = "CTRL|SHIFT",
+      action = wezterm.action.ActivateTab(2),
+    },
+    {
+      key = "9",
+      mods = "CTRL|SHIFT",
+      action = wezterm.action.ActivateTab(2),
+    },
+
+    -- Move between panes
+    {
+      key = "h",
+      mods = "CTRL|ALT",
+      action = wezterm.action.ActivatePaneDirection("Left"),
+    },
+    {
+      key = "l",
+      mods = "CTRL|ALT",
+      action = wezterm.action.ActivatePaneDirection("Right"),
+    },
+    {
+      key = "k",
+      mods = "CTRL|ALT",
+      action = wezterm.action.ActivatePaneDirection("Up"),
+    },
+    {
+      key = "j",
+      mods = "CTRL|ALT",
+      action = wezterm.action.ActivatePaneDirection("Down"),
+    },
+  }
+end
