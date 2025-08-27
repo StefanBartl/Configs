710f7e6527a90dc5d00fb196ce9924e829d5fb83
lavalue <bartl.stefan@pm.me>
2025-08-27 19:25:31 +0200

    upgrade and cleanup

--- Shells/oh-my-zsh/deprecated/fzf.zsh
+++
@@ -1,7 +0,0 @@
-# fzf Plugin
-# Provides a Fuzzy Finder for files and directories in your Zsh shell.
-
-# Activate the fzf plugin
-source /pfad/zur/fzf/plugin/fzf.zsh
-
-# Customize fzf behavior and keybindings in your Zsh configuration
--- Shells/oh-my-zsh/deprecated/tmux.zsh
+++
@@ -1,7 +0,0 @@
-# tmux Plugin
-# Integrates Zsh with tmux, a terminal multiplexer.
-
-# Activate the tmux plugin
-source /pfad/zur/tmux/plugin/tmux.zsh
-
-# Configure tmux settings and keybindings as needed
